803b623b1e6e9e37db804b074f53c146c8f62096
mahmoudsallam <mahmoudmostafa12396@gmail.com>
2021-09-28 21:31:07 +0200

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Mahmoud Sallam on 28/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
